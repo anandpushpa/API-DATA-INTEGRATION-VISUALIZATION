a64fe389a57a969f0cce450d2cd056e84cb86fc2
anandpushpa <saranyaanand2305@gmail.com>
2026-02-03 12:37:05 +0530

    Create weather_api.py

---
+++ weather_api.py
@@ -0,0 +1,34 @@
+ Internship Task Alternative – No API Required
+# Weather Data Visualization using Hardcoded Values
+
+import matplotlib.pyplot as plt
+
+# Hardcoded weather data (example)
+city = "Chennai"
+temperature = 32      # °C
+humidity = 70         # %
+pressure = 1015       # hPa
+wind_speed = 5.2      # m/s
+
+# Print weather details
+print("Weather Report")
+print("----------------")
+print("City Name :", city)
+print("Temperature :", temperature, "degree Celsius")
+print("Humidity :", humidity, "%")
+print("Pressure :", pressure, "hPa")
+print("Wind Speed :", wind_speed, "m/s")
+
+# Data for graph
+parameters = ["Temperature", "Humidity", "Pressure", "Wind Speed"]
+values = [temperature, humidity, pressure, wind_speed]
+
+# Plot bar graph
+plt.figure(figsize=(8,5))
+plt.bar(parameters, values, color=['red','blue','green','orange'])
+plt.title("Weather Data Visualization for " + city)
+plt.xlabel("Weather Parameters")
+plt.ylabel("Values")
+plt.show(block=True)
+plt.savefig("output.png")
+plt.show()
